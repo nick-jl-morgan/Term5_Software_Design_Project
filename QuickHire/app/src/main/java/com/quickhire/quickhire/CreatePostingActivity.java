08267517c93935c0f0fbc8ca1babc8d3c16ed429
nicholas.morgan <nick.jl.morgan@gmail.com>
2018-03-03 13:35:44 -0330

    Home screen

---
+++ QuickHire/app/src/main/java/com/quickhire/quickhire/CreatePostingActivity.java
@@ -0,0 +1,15 @@
+package com.quickhire.quickhire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class CreatePostingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_create_posting);
+    }
+}
