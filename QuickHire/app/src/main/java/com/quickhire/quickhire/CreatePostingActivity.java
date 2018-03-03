fde86aa56148b74bf2af5158ad731dd71009a1ce
mpik32 <34105275+Matthew-PI-King@users.noreply.github.com>
2018-03-03 13:56:32 -0500

    Bringing up to date

---
+++ QuickHire/app/src/main/java/com/quickhire/quickhire/CreatePostingActivity.java
@@ -0,0 +1,16 @@
+package com.quickhire.quickhire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class CreatePostingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_posting);
+    }
+
+
+}
--- QuickHire/app/src/main/java/com/quickhire/quickhire/MainActivity.java
+++ QuickHire/app/src/main/java/com/quickhire/quickhire/MainActivity.java
@@ -1,7 +1,10 @@
 package com.quickhire.quickhire;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureCreatePostButton();
+    }
+    private void configureCreatePostButton() {
+        Button postButton = (Button) findViewById(R.id.createJobPostingButton);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CreatePostingActivity.class));
+            }
+        });
     }
 }
+
