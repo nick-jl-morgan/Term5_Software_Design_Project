ba63b325bc2c85ea0c5a87b919bcd5336838a349
nicholas.morgan <nick.jl.morgan@gmail.com>
2018-03-06 18:39:47 -0330

    Commiting and Merging to master

--- QuickHire/app/src/main/java/com/quickhire/quickhire/DatabaseConnection.java
+++ QuickHire/app/src/main/java/com/quickhire/quickhire/DatabaseConnection.java
@@ -1,10 +1,5 @@
 package com.quickhire.quickhire;
 
-
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-
 import java.io.IOException;
 
 
@@ -13,19 +8,5 @@ import java.io.IOException;
  */
 
 public interface DatabaseConnection {
-
-    OkHttpClient client = new OkHttpClient();
-
-    Request request = new Request.Builder()
-            .url("http://192.168.0.27:5000/API/")
-            .build();
-
-    public static final MediaType JSON
-            = MediaType.parse("application/json; charset=utf-8");
-
-    public String GetUser(String username, String password);
-
-    public String doPostRequest(String json) throws IOException;
-
 
 }
